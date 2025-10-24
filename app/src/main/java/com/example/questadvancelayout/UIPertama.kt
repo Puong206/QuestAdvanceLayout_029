dfd728ce2addeddd2a628452748dc2c8efb7e37d
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-10-24 15:44:37 +0700

    Menambahkan Card di bawah Spacer

--- app/src/main/java/com/example/questadvancelayout/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout/UIPertama.kt
@@ -1,25 +1,28 @@
 package com.example.questadvancelayout
-
+import androidx.compose.*
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import java.lang.reflect.Modifier
 
 
 @Composable
 fun ActivityPertama(modifier: Modifier) {
     Column(modifier= Modifier
-        .padding(top=100.dp)
+        .padding(top = 100.dp)
         .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ){
@@ -35,5 +38,16 @@ fun ActivityPertama(modifier: Modifier) {
         )
 
         Spacer(modifier = Modifier.height(height = 25.dp))
+        Card(
+            modifier = Modifier
+                .height(height = 160.dp)
+                .fillMaxWidth(fraction = 1f)
+                .padding(all = 12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.DarkGray
+            )
+        ){
+
+        }
     }
 }
