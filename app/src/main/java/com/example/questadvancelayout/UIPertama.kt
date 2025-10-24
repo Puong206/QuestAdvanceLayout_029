9765a01adec754c23e56321724e76b0a32a584c5
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-10-24 15:27:36 +0700

    Membuat fungsi composable ActivityPertama

--- app/src/main/java/com/example/questadvancelayout/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout/UIPertama.kt
@@ -1,2 +1,10 @@
 package com.example.questadvancelayout
 
+import androidx.compose.runtime.Composable
+import java.lang.reflect.Modifier
+
+
+@Composable
+fun ActivityPertama(modifier: Modifier) {
+
+}
