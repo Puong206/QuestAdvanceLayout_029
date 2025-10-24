5b822e44941c43f930bd33862b9f4edad1a1e29f
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-10-24 22:15:01 +0700

    Membuat Column

--- app/src/main/java/com/example/questadvancelayout/ActivityPraktikum.kt
+++ app/src/main/java/com/example/questadvancelayout/ActivityPraktikum.kt
@@ -1,9 +1,21 @@
 package com.example.questadvancelayout
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.background
 
 @Composable
 fun ActivityPraktikum (modifier: Modifier) {
+    Column (modifier = Modifier
+                .padding(top = 100.dp)
+                .fillMaxSize()
+                .background(color = colorResource(id = R.color.blue))
+    ) {
 
+    }
 }
--- app/src/main/java/com/example/questadvancelayout/MainActivity.kt
+++ app/src/main/java/com/example/questadvancelayout/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestAdvanceLayoutTheme {
                 Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    ActivityPertama(
+                    ActivityPraktikum(
 
                         modifier = Modifier.padding(innerPadding)
                     )
