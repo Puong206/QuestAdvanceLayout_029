f34ec318a11988699349b31b7f490bac39e3bae5
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-10-24 15:34:33 +0700

    Menambahkan Spacer di antara teks

--- app/src/main/java/com/example/questadvancelayout/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout/UIPertama.kt
@@ -1,7 +1,9 @@
 package com.example.questadvancelayout
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -31,5 +33,7 @@ fun ActivityPertama(modifier: Modifier) {
             text = stringResource(id= R.string.univ),
             fontSize = 18.sp
         )
+
+        Spacer(modifier = Modifier.height(height = 25.dp))
     }
 }
