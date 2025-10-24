b06c7be75879637b9fe3db4b6270ceec77f27c0e
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-10-24 16:03:38 +0700

    Menambahkan nama pada card composable

--- app/src/main/java/com/example/questadvancelayout/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout/UIPertama.kt
@@ -1,5 +1,6 @@
 package com.example.questadvancelayout
 import androidx.compose.*
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -7,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -14,7 +16,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -67,7 +71,15 @@ fun ActivityPertama(modifier: Modifier) {
                     color = Color.White,
                     modifier = Modifier.padding(top = 15.dp)
                 )
+                Text (
+                    text = stringResource(id = R.string.nama),
+                    fontSize = 20.sp,
+                    fontFamily = FontFamily.Cursive,
+                    color = Color.Yellow,
+                    modifier = Modifier.padding(top = 10.dp)
+                )
             }
         }
+
     }
 }
