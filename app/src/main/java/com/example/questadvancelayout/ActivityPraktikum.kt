346cf9337a0349a506fa242ff32fba122be84da9
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-10-24 22:36:10 +0700

    Menambahkan Teks Nama di Tengah Layar

--- app/src/main/java/com/example/questadvancelayout/ActivityPraktikum.kt
+++ app/src/main/java/com/example/questadvancelayout/ActivityPraktikum.kt
@@ -6,10 +6,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.foundation.background
+import androidx.compose.material3.Text
 
 @Composable
 fun ActivityPraktikum (modifier: Modifier) {
@@ -17,8 +22,18 @@ fun ActivityPraktikum (modifier: Modifier) {
         modifier = Modifier.fillMaxSize(),
         color = colorResource(id = R.color.blue)
     ) {
-        Column {
-
+        Column (modifier = Modifier
+            .padding(top = 100.dp)
+            .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = stringResource(id = R.string.nama),
+                //fontFamily = ,
+                fontSize = 32.sp,
+                color = colorResource(id = R.color.white),
+                fontWeight = FontWeight.Bold
+            )
         }
     }
 }
