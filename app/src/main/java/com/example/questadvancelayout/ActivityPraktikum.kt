165a211ff18949912ebe76b1b32b83534b15bc45
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-10-24 23:50:25 +0700

    Menambahkan gambar profil dengan border dan clip berbentuk lingkaran

--- app/src/main/java/com/example/questadvancelayout/ActivityPraktikum.kt
+++ app/src/main/java/com/example/questadvancelayout/ActivityPraktikum.kt
@@ -1,5 +1,7 @@
 package com.example.questadvancelayout
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.material3.Surface
 import androidx.compose.ui.graphics.Color
 import androidx.compose.foundation.layout.Column
@@ -15,7 +17,12 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+
 
 @Composable
 fun ActivityPraktikum (modifier: Modifier) {
@@ -24,18 +31,31 @@ fun ActivityPraktikum (modifier: Modifier) {
         color = colorResource(id = R.color.blue)
     ) {
         Column (modifier = Modifier
-            .padding(top = 100.dp)
+            .padding(top = 80.dp)
             .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-
+            val gambar = painterResource(id = R.drawable.profil_kereta)
+            Image(painter = gambar,
+                contentDescription = null,
+                contentScale = ContentScale.Fit,
+                modifier = Modifier
+                    .padding(all = 10.dp)
+                    .height(height = 160.dp)
+                    .border(
+                        width = 2.dp,
+                        color = Color.White,
+                        shape = CircleShape
+                    )
+                    .clip(CircleShape))
+            Spacer(modifier = Modifier.height(height = 8.dp))
             Text(
                 text = stringResource(id = R.string.nama),
                 fontSize = 32.sp,
                 color = colorResource(id = R.color.white),
                 fontWeight = FontWeight.Bold
             )
-            Spacer(modifier = Modifier.height(height = 8.dp))
+            Spacer(modifier = Modifier.height(height = 4.dp))
             Text(
                 text = stringResource(id = R.string.username),
                 fontSize = 20.sp,
